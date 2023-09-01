cdfa168121fdccf9230f92a02d9703a0242cadfd
Jael <jhael.nicolalde@epn.edu.ec.com>
2023-09-01 16:17:20 -0500

    updated credentials

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,30 +15,47 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //Añade usuario administrador
-        DB::table('users')->insert([
-            'name' => '---',
-            'email' => 'adminEmail@example.com',
-            'password' => Hash::make('administrador'),
-            'cedula' => '---',
-            'celular' => '---',
-            'fnac' => '---',
-            'direccion' => '---',
-            'role_id' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        [
-            'name' => '---',
-            'email' => 'cliEmail@example.com',
-            'password' => Hash::make('12345'),
-            'cedula' => '---',
-            'celular' => '---',
-            'fnac' => '---',
-            'direccion' => '---',
-            'role_id' => 2,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // Añade usuarios
+        $users = [
+            [
+                'name' => '---',
+                'email' => 'adminEmail@example.com',
+                'password' => Hash::make('administrador'),
+                'cedula' => '---',
+                'celular' => '---',
+                'fnac' => '---',
+                'direccion' => '---',
+                'role_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => '---',
+                'email' => 'jane.smith@example.com',
+                'password' => Hash::make('ClientePass456'),
+                'cedula' => '---',
+                'celular' => '---',
+                'fnac' => '---',
+                'direccion' => '---',
+                'role_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => '---',
+                'email' => 'john.doe@example.com',
+                'password' => Hash::make('EmpleadoPass123'),
+                'cedula' => '---',
+                'celular' => '---',
+                'fnac' => '---',
+                'direccion' => '---',
+                'role_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ];
+
+        // Inserta los usuarios en la base de datos
+        DB::table('users')->insert($users);
     }
 }
